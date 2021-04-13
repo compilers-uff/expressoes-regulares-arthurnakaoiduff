c1c388ea8ea018155d6d2311652a8605ae765082
Arthur Nakao <arthur.nakao@gmail.com>
2021-04-12 23:46:32 -0300

    Atualizando arquivo principal para ser so responsavel por receber a entrada e retornar a saida

--- er.py
+++ er.py
@@ -1,19 +1,5 @@
 import sys
-from Classes.afne import *
-from Classes.er import *
-
-def erToAFNe(er):
-  erInstance = Er.getEr(er)
-  return erInstance.equivalentAfne(erInstance.queue.pop(0))
-
-def afneToAFN(Afne):
-  return Afne.equivalentAfn()
-
-def afnToAFD(Afn):
-  return Afn.equivalentAfd()
-
-def afdToAFDmin(Afd):
-  return Afd.minimize()
+from mainFunctions import *
 
 if(sys.argv[1] == "-f"):
   filename = sys.argv[2]
